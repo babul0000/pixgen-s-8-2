07459734e4db2a002a55da79e9d89fd624554906
Your Name <babulhossan.info@gmail.com>
2026-04-28 20:16:28 +0600

    done

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const HomePage = async () => {
-    const res = await fetch("https://pixgen-b.vercel.app/data.json");
-    const data = await res.json();
-    console.log(data);
+    // const res = await fetch("https://pixgen-b.vercel.app/data.json");
+    // const data = await res.json();
+    // console.log(data);
 
     return (
         <div>
